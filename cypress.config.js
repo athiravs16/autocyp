6a33778f4c817dafbb1e3a0c85cccc5d0231854b
athiravs16 <athiravs016@gmail.com>
2022-12-09 15:33:17 +0530

    Changes in Cypress Project

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     specPattern: 'cypress/e2e/*.js'
+
   },
+ 
 });
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -13,52 +13,96 @@
     {cy.log(lg.text())
      }) 
      const gl=cy.get('.logo')
-    cy.get('#loginSignup').click()
-
-    cy.get('.logo').should('be.visible')
-  
-    cy.get('.mb-0').should('have.text','Plans and Prices')
-    cy.get('.mt0').should('be.visible')
-    
-    cy.get('.container:visible').should('have.length',6)
-    
-    cy.get('.jfHeader-menuListItem')
-    cy.get('.jfHeader-menuListItem:visible').should('have.length',7)
-    
-    cy.get('.jfHeader-content').should('be.visible')
-
     cy.get('.jfHeader-logoWrapper').should('exist')
     cy.get('.jfHeader-menuWrapper').contains('Pricing').should('exist')
     cy.get('.jfHeader-menuListLink:visible').should('exist')
     cy.wait(2000)
     cy.get('.js-missingAccountInfo.desktop-hidden').should('not.be.visible')
-    
-    cy.get('#jfHeader-userProfile').click({force:true})
-    cy.get('.jfHeader-menuList:visible').click({multiple:true})
-    
-    cy.get('.jfHeader-menuList').find('.jfHeader-menuListItem:visible').should('have.length',7)
 
-    cy.get('.sticky').should('be.visible')
+
+     cy.get(':nth-child(4) > .desktop-hidden > .jfHeader-menuListLink').should('have.attr','href','https://phptravels.com/demo')
+     cy.get(':nth-child(4) > .desktop-hidden > .jfHeader-menuListLink').click({force:true})
+     cy.go('back')
+     
+     cy.get('.jfHeader-menuListItem:visible').should('have.length',7)
+     cy.get('.jfHeader-content').should('be.visible')
+     cy.get(':nth-child(3) > .jfHeader-menuListLink').should('have.text','Features')
+     cy.get('.jfHeader-menuListLink:visible').eq(2).contains('Features').click()
+     cy.get(':nth-child(4) > .jfHeader-menuListLink').should('have.text','Integrations')
+     cy.get('.jfHeader-menuListLink:visible').eq(3).contains('Integrations').click()
+     cy.get('.jfHeader-support-menu > .jfHeader-menuListLink').should('have.text','Company')
+     cy.get('.jfHeader-menuListLink:visible').eq(4).contains('Company').click({force:true})
+     cy.get('.jfHeader-menuListLink').eq(2).should('be.visible')
+     cy.get('.jfHeader-menuListLink').eq(3).should('be.visible')
+     cy.get('.jfHeader-menuListLink').eq(4).should('be.visible')
+     
+    cy.get('.signup-btn-wrapper > .jfHeader-menuListLink').should('have.attr','href','https://phptravels.org/register.php')
+    cy.get('.signup-btn-wrapper > .jfHeader-menuListLink').invoke('removeAttr','target').click()
+    cy.go('back')
+    cy.get('.login-btn-wrapper > .jfHeader-menuListLink').should('have.attr','href','https://phptravels.org/login')
+    cy.get('.login-btn-wrapper > .jfHeader-menuListLink').invoke('removeAttr','target').click()
+    cy.go('back')
+    cy.get('.logo').should('be.visible')
+  
+    cy.get('.mb-0').should('have.text','Plans and Prices')
+    cy.get('.mt0').should('be.visible')
+    cy.get('.princing-section').should('be.visible')
+
+    cy.get(':nth-child(1) > .pricing-package > :nth-child(1) > small').should('be.visible')
+    cy.get(':nth-child(2) > .pricing-package > :nth-child(1) > small').should('be.visible')
+    cy.get(':nth-child(3) > .pricing-package > :nth-child(1) > small').should('be.visible')
+    cy.get(':nth-child(4) > .pricing-package > :nth-child(1) > small').should('be.visible')
+    cy.get('.strong').should('have.length',8)
+    cy.get('.pricing-package-name').should('have.length',4) 
+    cy.get('.container:visible').should('have.length',6)  
+    cy.get('#content').should('be.visible')
+
+
+    cy.get(':nth-child(1) > .pricing-package > .pricing-package-price').should('be.visible')
+    cy.get(':nth-child(2) > .pricing-package > .pricing-package-price').should('be.visible')
+    cy.get(':nth-child(3) > .pricing-package > .pricing-package-price').should('be.visible')
+    cy.get(':nth-child(4) > .pricing-package > .pricing-package-price').should('be.visible')
     
+  
+    cy.get('.jfHeader-menuList').find('.jfHeader-menuListItem:visible').should('have.length',7)
     cy.wait(4000)
-    cy.get('.pricing-package-name').eq(0).should('have.text','Startup')
-    cy.get('.pricing-package-name').eq(1).should('have.text','Agency')
-    cy.get('.pricing-package-name').eq(2).should('have.text','Business')
-    cy.get('.pricing-package-name').eq(3).should('have.text','Enterprise')
+    cy.get(':nth-child(1) > .pricing-package > .pricing-package-name > strong').should('have.text','Startup')
+    cy.get(':nth-child(2) > .pricing-package > .pricing-package-name > strong').should('have.text','Agency')
+    cy.get(':nth-child(3) > .pricing-package > .pricing-package-name > strong').should('have.text','Business')
+    cy.get(':nth-child(4) > .pricing-package > .pricing-package-name > strong').should('have.text','Enterprise')
     cy.get('.pricing-package').should('be.visible')
     cy.get('.col-md-3').should('exist')
 
     cy.get('.df:visible').should('exist')
 
+
     cy.get('.df:visible').eq(1).should('exist')
     cy.get('.df:visible').eq(2).should('exist')
+    
+    cy.get('[data-wow-delay="1.4s"] > .feature-svg').should('be.visible')
+    cy.get('.logos').should('be.visible')
+    cy.get('.row').should('be.visible')
+
+    cy.get(':nth-child(1) > .pricing-package > .pricing-package-price').should('have.text','$50 / Monthly')
    
 
-    cy.get('.btn.subscribe').eq(0).contains('Subscribe').click()
-    cy.get('.btn.subscribe').eq(1).contains('Subscribe').click()
-    cy.get('.btn.subscribe').eq(2).contains('Subscribe').click({force:true})
-    cy.contains('Buy Now').click()
-    cy.get('.btn').should('exist')
+    })
+
+    it('My second Test Case', () => {
+      cy.visit('https://phptravels.com/pricing')
+      cy.get(':nth-child(1) > .pricing-package > .sticky > .btn').should('have.attr','href','https://link.phptravels.com/startup')
+      cy.get(':nth-child(1) > .pricing-package > .sticky > .btn').invoke('removeAttr','target').click({force:true})
+      cy.go('back')
+      cy.get(':nth-child(2) > .pricing-package > .sticky > .btn').should('have.attr','href','https://link.phptravels.com/agency')
+      cy.get(':nth-child(2) > .pricing-package > .sticky > .btn').invoke('removeAttr','target').click()
+      cy.go('back')
+      cy.get(':nth-child(3) > .pricing-package > .sticky > .btn').should('have.attr','href','https://link.phptravels.com/business')
+      cy.get(':nth-child(3) > .pricing-package > .sticky > .btn').invoke('removeAttr','target').click()
+      cy.go('back')
+      cy.get(':nth-child(4) > .pricing-package > .sticky > .btn').should('have.attr','href','https://link.phptravels.com/enterprise')
+      cy.get(':nth-child(4) > .pricing-package > .sticky > .btn').invoke('removeAttr','target').click()
+      cy.go('back')
+      cy.get('.btn').should('exist')
     
   
   })
